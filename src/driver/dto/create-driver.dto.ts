d651f295e5630657e4f224a40c8d38386bf4ebf6
kenbme <guilherme.barbosa@ccc.ufcg.edu.br>
2024-03-04 13:41:35 -0300

    driver must be userType DRIVER

--- src/driver/dto/create-driver.dto.ts
+++ src/driver/dto/create-driver.dto.ts
@@ -1,3 +1,9 @@
+import { Equals, IsString } from 'class-validator'
 import {CreateUserDto} from 'src/user/dto/create-user.dto'
+import { UserTypeEnum } from 'src/user/enums/user-type.enum'
 
-export class CreateDriverDto extends CreateUserDto {}
+export class CreateDriverDto extends CreateUserDto {
+    @IsString()
+    @Equals(UserTypeEnum.DRIVER)
+    userType: string
+}
