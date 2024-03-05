e2e4bb131391816ca231c72ee9e745874bf750b2
Vinicius <viniipvp@gmail.com>
2024-03-05 14:09:29 -0300

    establishmentDTO

--- src/establishment/dto/create-establishment.dto.ts
+++ src/establishment/dto/create-establishment.dto.ts
@@ -1,3 +1,9 @@
+import { IsString, Equals } from 'class-validator'
 import {CreateUserDto} from 'src/user/dto/create-user.dto'
+import { UserTypeEnum } from 'src/user/enums/user-type.enum'
 
-export class CreateEstablishmentDto extends CreateUserDto {}
+export class CreateEstablishmentDto extends CreateUserDto {
+    @IsString()
+    @Equals(UserTypeEnum.ESTABLISHMENT)
+    userType: string
+}
