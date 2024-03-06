4a37cf7e1296089ee94cf43667a75519dbb92148
kenbme <guilherme.barbosa@ccc.ufcg.edu.br>
2024-03-05 23:35:05 -0300

    auth spec init

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthService } from './auth.service'
 import { User } from 'src/user/entities/user.entity'
 import { UserType } from 'src/user/entities/user.type.entity'
+import { JwtModule } from '@nestjs/jwt'
+import { jwtConstants } from './constants'
 
 describe('AuthController', () => {
   let controller: AuthController
@@ -16,15 +18,19 @@ describe('AuthController', () => {
         database: 'db/testing.sqlite3',
         synchronize: true,
         entities: [User, UserType]
+      }), JwtModule.register({
+        global: true,
+        secret: jwtConstants.secret,
+        signOptions: {expiresIn: '60s'}
       }), UserModule],
-      controllers: [],
-      providers: []
+      controllers: [AuthController],
+      providers: [AuthService]
     }).compile()
 
-    //controller = module.get<AuthController>(AuthController)
+    controller = module.get<AuthController>(AuthController)
   })
 
   it('should be defined', () => {
-    //expect(controller).toBeDefined()
+    expect(controller).toBeDefined()
   })
 })
