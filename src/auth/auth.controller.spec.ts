80bebb729e140ac57c570abfdeffb8ca06fbe958
kenbme <guilherme.barbosa@ccc.ufcg.edu.br>
2024-03-06 00:16:04 -0300

    criando caso simples de registro e login

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,36 +1,71 @@
 import {Test, TestingModule} from '@nestjs/testing'
 import {AuthController} from './auth.controller'
-import { UserModule } from 'src/user/user.module'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { AuthService } from './auth.service'
-import { User } from 'src/user/entities/user.entity'
-import { UserType } from 'src/user/entities/user.type.entity'
-import { JwtModule } from '@nestjs/jwt'
-import { jwtConstants } from './constants'
+import {UserModule} from 'src/user/user.module'
+import {TypeOrmModule, getRepositoryToken} from '@nestjs/typeorm'
+import {AuthService} from './auth.service'
+import {User} from 'src/user/entities/user.entity'
+import {UserType} from 'src/user/entities/user.type.entity'
+import {JwtModule} from '@nestjs/jwt'
+import {jwtConstants} from './constants'
+import {Repository} from 'typeorm'
+import {UserService} from 'src/user/user.service'
 
 describe('AuthController', () => {
-  let controller: AuthController
+  let authController: AuthController
+  let userRepository: Repository<User>
+  let userService: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forRoot({
-        type: 'sqlite',
-        database: 'db/testing.sqlite3',
-        synchronize: true,
-        entities: [User, UserType]
-      }), JwtModule.register({
-        global: true,
-        secret: jwtConstants.secret,
-        signOptions: {expiresIn: '60s'}
-      }), UserModule],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: 'db/testing.sqlite3',
+          synchronize: true,
+          entities: [User, UserType]
+        }),
+        TypeOrmModule.forFeature([User]),
+        JwtModule.register({
+          global: true,
+          secret: jwtConstants.secret,
+          signOptions: {expiresIn: '60s'}
+        }),
+        UserModule
+      ],
       controllers: [AuthController],
-      providers: [AuthService]
+      providers: [AuthService, {provide: getRepositoryToken(User), useClass: Repository}]
     }).compile()
 
-    controller = module.get<AuthController>(AuthController)
+    authController = module.get(AuthController)
+    userService = module.get(UserService)
+    userRepository = module.get(getRepositoryToken(User))
+    await userRepository.clear()
   })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined()
+    expect(authController).toBeDefined()
+    expect(userService).toBeDefined()
+    expect(userRepository).toBeDefined()
+  })
+
+  it('should create user and login', async () => {
+    const count = await userRepository.count()
+    const user = await userService.create({
+      username: '222.222.222-22',
+      password: 'senhaDificil123!',
+      email: 'teste@gmail.com',
+      birthday: '2002-12-12',
+      name: 'fulano',
+      userType: 'DRIVER'
+    })
+    const newCount = await userRepository.count()
+    expect(newCount).toBe(count + 1)
+    expect(user.name).toBe('fulano')
+
+    const token = await authController.signIn({
+      username: '222.222.222-22',
+      password: 'senhaDificil123!'
+    })
+    expect(token).toBeDefined()
   })
 })
--- src/establishment/establishment.controller.spec.ts
+++ src/establishment/establishment.controller.spec.ts
@@ -1,25 +1,29 @@
 import {Test, TestingModule} from '@nestjs/testing'
 import {EstablishmentController} from './establishment.controller'
 import {EstablishmentService} from './establishment.service'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { User } from 'src/user/entities/user.entity'
-import { UserType } from 'src/user/entities/user.type.entity'
-import { Establishment } from './entities/establishment.entity'
-import { UserModule } from 'src/user/user.module'
-import { EstablishmentType } from './entities/establishment.type.entity'
-import { EstablishmentTypeService } from './establishment.type.service'
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {User} from 'src/user/entities/user.entity'
+import {UserType} from 'src/user/entities/user.type.entity'
+import {Establishment} from './entities/establishment.entity'
+import {UserModule} from 'src/user/user.module'
+import {EstablishmentType} from './entities/establishment.type.entity'
+import {EstablishmentTypeService} from './establishment.type.service'
 
 describe('EstablishmentController', () => {
   let controller: EstablishmentController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forRoot({
-        type: 'sqlite',
-        database: 'db/testing.sqlite3',
-        synchronize: true,
-        entities: [User, UserType, Establishment, EstablishmentType]
-      }), TypeOrmModule.forFeature([Establishment, EstablishmentType]), UserModule],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: 'db/testing.sqlite3',
+          synchronize: true,
+          entities: [User, UserType, Establishment, EstablishmentType]
+        }),
+        TypeOrmModule.forFeature([Establishment, EstablishmentType]),
+        UserModule
+      ],
       controllers: [EstablishmentController],
       providers: [EstablishmentService, EstablishmentTypeService]
     }).compile()
