887711b52b75e7670852cb9ce44fc19261f86b36
kenbme <guilherme.barbosa@ccc.ufcg.edu.br>
2024-03-05 23:41:29 -0300

    driver spec init

--- src/driver/driver.controller.spec.ts
+++ src/driver/driver.controller.spec.ts
@@ -1,12 +1,23 @@
 import {Test, TestingModule} from '@nestjs/testing'
 import {DriverController} from './driver.controller'
 import {DriverService} from './driver.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Driver } from './entities/driver.entity'
+import { UserType } from 'src/user/entities/user.type.entity'
+import { User } from 'src/user/entities/user.entity'
+import { UserModule } from 'src/user/user.module'
 
 describe('DriverController', () => {
   let controller: DriverController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot({
+        type: 'sqlite',
+        database: 'db/testing.sqlite3',
+        synchronize: true,
+        entities: [User, UserType, Driver]
+      }), TypeOrmModule.forFeature([Driver]), UserModule],
       controllers: [DriverController],
       providers: [DriverService]
     }).compile()
