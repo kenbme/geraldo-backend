439b71606a7976026a0c65890b51c9151f245874
joaopedrocporto <jpcamposporto@gmail.com>
2024-03-07 18:55:38 -0300

    'correcao'

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,5 +1,5 @@
-import { IsString, ValidationArguments, ValidationOptions, registerDecorator } from 'class-validator';
-import { User } from 'src/user/entities/user.entity';
+import { IsString, ValidationArguments, ValidationOptions, registerDecorator } from 'class-validator'
+import { User } from 'src/user/entities/user.entity'
 
 export class LoginDTO {
   @IsString()
@@ -9,15 +9,17 @@ export class LoginDTO {
 }
 
 export function isCPF(validationOptions?: ValidationOptions) {
-  return function (object: User, propertyName: User["username"]) {
+  return function (object: User, propertyName: keyof User) {
     registerDecorator({
       name: 'isCPF',
       target: object.constructor,
-      propertyName: propertyName,
+      propertyName: propertyName as string,
       options: validationOptions,
       validator: {
         validate(value: string, args: ValidationArguments) {
-          if(value.length !== 11  ){ return false}
+          if (value.length !== 11) {
+            return false
+          }
           let produto1 = 0
           let produto2 = 0
           let valorD1 = 0
@@ -28,36 +30,39 @@ export function isCPF(validationOptions?: ValidationOptions) {
             produto1 += parseInt(value.charAt(i)) * (10 - i)
             produto2 += parseInt(value.charAt(i)) * (11 - i)
           }
-          produto2 += digito1 * (2)
-          if((produto1 % 11 < 2)){
+          produto2 += digito1 * 2
+          if (produto1 % 11 < 2) {
             valorD1 = 0
-          }else{
-            valorD1 = 11- produto1%11
+          } else {
+            valorD1 = 11 - (produto1 % 11)
           }
-          if((produto2%11 < 2)){
+          if (produto2 % 11 < 2) {
             valorD2 = 0
-          }else{
-            valorD2 = 11- produto2%11
+          } else {
+            valorD2 = 11 - (produto2 % 11)
           }
-          if(digito1 !== valorD1 || digito2 !== valorD2){return false}
+          if (digito1 !== valorD1 || digito2 !== valorD2) {
+            return false
+          }
           return true
-        },
-      },
-    });
-  };
+        }
+      }
+    })
+  }
 }
 
-
 export function isCNPJ(validationOptions?: ValidationOptions) {
-  return function (object: User, propertyName: User["username"]) {
+  return function (object: User, propertyName: keyof User) {
     registerDecorator({
       name: 'isCNPJ',
       target: object.constructor,
-      propertyName: propertyName,
+      propertyName: propertyName as string,
       options: validationOptions,
       validator: {
         validate(value: string, args: ValidationArguments) {
-          if( value.length != 14 ){ return false}
+          if (value.length != 14) {
+            return false
+          }
           let produto1 = 0
           let produto2 = 0
           let valorD1 = 0
@@ -69,22 +74,23 @@ export function isCNPJ(validationOptions?: ValidationOptions) {
             produto1 += parseInt(value.charAt(i)) * pesos[i]
             produto2 += parseInt(value.charAt(i)) * (pesos[i] + i)
           }
-          produto2 += digito1 * (2)
-          if((produto1%11 < 2)){
+          produto2 += digito1 * 2
+          if (produto1 % 11 < 2) {
             valorD1 = 0
-          }else{
-            valorD1 = 11- produto1%11
+          } else {
+            valorD1 = 11 - (produto1 % 11)
           }
-          if((produto2%11 < 2)){
+          if (produto2 % 11 < 2) {
             valorD2 = 0
-          }else{
-            valorD2 = 11- produto2%11
+          } else {
+            valorD2 = 11 - (produto2 % 11)
           }
-          if(digito1 !== valorD1 || digito2 !== valorD2){return false}
+          if (digito1 !== valorD1 || digito2 !== valorD2) {
+            return false
+          }
           return true
-        },
-      },
-    });
-  };
+        }
+      }
+    })
+  }
 }
-
