dce20a8e510995e269031683711b626741282d51
kenbme <guilherme.barbosa@ccc.ufcg.edu.br>
2024-03-08 14:57:12 -0300

    Padronizando handlers de Response

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,8 +8,13 @@ import {Establishment} from './establishment/entities/establishment.entity'
 import {UserType} from './user/entities/user.type.entity'
 import {EstablishmentType} from './establishment/entities/establishment.type.entity'
 import {CepModule} from './cep/cep.module'
-import { DriverModule } from './driver/driver.module'
-import { EstablishmentModule } from './establishment/establishment.module'
+import {DriverModule} from './driver/driver.module'
+import {EstablishmentModule} from './establishment/establishment.module'
+import {APP_FILTER, APP_INTERCEPTOR} from '@nestjs/core'
+import {ResponseInterceptor} from './response.interceptor'
+import {BadRequestExceptionFilter} from './badrequest.filter'
+import {HttpExceptionExceptionFilter} from './httpexception.filter '
+import {TypeORMExceptionFilter} from './typeorm.filter '
 
 @Module({
   imports: [
@@ -24,6 +29,24 @@ import { EstablishmentModule } from './establishment/establishment.module'
     CepModule,
     DriverModule,
     EstablishmentModule
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionExceptionFilter
+    },
+    {
+      provide: APP_FILTER,
+      useClass: BadRequestExceptionFilter
+    },
+    {
+      provide: APP_FILTER,
+      useClass: TypeORMExceptionFilter
+    }
   ]
 })
 export class AppModule {}
---
+++ src/httpexception.filter .ts
@@ -0,0 +1,20 @@
+import {ArgumentsHost, Catch, ExceptionFilter, HttpException} from '@nestjs/common'
+
+@Catch(HttpException)
+export class HttpExceptionExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const context = host.switchToHttp()
+    const response = context.getResponse()
+
+    const statusCode = exception.getStatus()
+    const message: any = exception.getResponse()
+
+    response.status(statusCode).send(
+      {
+        statusCode,
+        message: message.message,
+        data: message.error
+      }
+    )
+  }
+}
