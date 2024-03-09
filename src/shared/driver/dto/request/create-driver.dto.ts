3057f53f7e67ddf059c5fa2715eb1e53597d6987
João Victor <jvdso.15@gmail.com>
2024-03-09 17:33:04 -0300

    fix : alteração nas mensagens do dto

--- src/shared/driver/dto/request/create-driver.dto.ts
+++ src/shared/driver/dto/request/create-driver.dto.ts
@@ -5,14 +5,14 @@ export class CreateDriverDto {
   @IsCPF()
   username: string
 
-  @IsString({message: 'O nome deve ser válido'})
+  @IsString({message: 'Nome inválido'})
   @MinLength(4, {message: 'O nome deve ter pelo menos 4 caracteres'})
   name: string
 
-  @IsEmail({}, {message: 'O email deve ser um email válido'})
+  @IsEmail({}, {message: 'Email inválido'})
   email: string
 
-  @IsNotEmpty({message: 'A data de nascimento não pode ser vazia'})
+  @IsNotEmpty({message: 'Data de nascimento inválida'})
   @IsDateString({}, {message: 'A data de nascimento deve estar em um formato válido'})
   birthday: string
 }
