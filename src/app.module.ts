b7ea3df234b3be2d7eab63d2cf4e242e7b45a3bc
kenbme <guilherme.barbosa@ccc.ufcg.edu.br>
2024-03-13 00:50:19 -0300

    criando authguard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,23 @@
-import { Module } from '@nestjs/common'
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { AuthModule } from './auth/auth.module'
-import { BadRequestExceptionFilter } from './badrequest.filter'
-import { CepModule } from './cep/cep.module'
-import { DriverModule } from './driver/driver.module'
-import { Driver } from './driver/entities/driver.entity'
-import { Establishment } from './establishment/entities/establishment.entity'
-import { EstablishmentType } from './establishment/entities/establishment.type.entity'
-import { EstablishmentModule } from './establishment/establishment.module'
-import { HttpExceptionExceptionFilter } from './httpexception.filter '
-import { ResponseInterceptor } from './response.interceptor'
-import { TypeORMExceptionFilter } from './typeorm.filter '
-import { User } from './user/entities/user.entity'
-import { UserType } from './user/entities/user.type.entity'
-import { UserModule } from './user/user.module'
-import { Vehicle } from './vehicle/entities/vehicle.entity'
-import { VehicleModule } from './vehicle/vehicle.module'
+import {Module} from '@nestjs/common'
+import {APP_FILTER, APP_GUARD, APP_INTERCEPTOR} from '@nestjs/core'
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {AuthModule} from './auth/auth.module'
+import {BadRequestExceptionFilter} from './badrequest.filter'
+import {CepModule} from './cep/cep.module'
+import {DriverModule} from './driver/driver.module'
+import {Driver} from './driver/entities/driver.entity'
+import {Establishment} from './establishment/entities/establishment.entity'
+import {EstablishmentType} from './establishment/entities/establishment.type.entity'
+import {EstablishmentModule} from './establishment/establishment.module'
+import {HttpExceptionExceptionFilter} from './httpexception.filter '
+import {ResponseInterceptor} from './response.interceptor'
+import {TypeORMExceptionFilter} from './typeorm.filter '
+import {User} from './user/entities/user.entity'
+import {UserType} from './user/entities/user.type.entity'
+import {UserModule} from './user/user.module'
+import {Vehicle} from './vehicle/entities/vehicle.entity'
+import {VehicleModule} from './vehicle/vehicle.module'
+import {AuthGuard} from './authguard'
 
 @Module({
   imports: [
@@ -49,6 +50,10 @@ import { VehicleModule } from './vehicle/vehicle.module'
     {
       provide: APP_FILTER,
       useClass: TypeORMExceptionFilter
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
     }
   ]
 })
