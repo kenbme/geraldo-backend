22ecf8bc2159baffdfc4f980a5ee7e5d9d70b982
joaopedrocporto <jpcamposporto@gmail.com>
2024-03-19 18:33:21 -0300

    correcao dto

--- src/shared/vehicle/dto/request/create-vehicle.dto.ts
+++ src/shared/vehicle/dto/request/create-vehicle.dto.ts
@@ -1,18 +1,18 @@
-import {IsInt, IsNotEmpty, IsString, Min} from 'class-validator'
-import {isValidYear} from '../../validators/isValidYear'
+import { IsInt, IsNotEmpty, IsString, Min } from 'class-validator'
+import { isValidYear } from '../../validators/isValidYear'
 
 export class CreateVehicleDto {
-  @IsString()
+  @IsString({message: 'O modelo precisa ser uma string'})
   @IsNotEmpty({message: 'O modelo não pode ser vazio'})
   model: string
 
-  @IsString()
+  @IsString({message: 'A plca precisa ser uma string'})
   @IsNotEmpty({message: 'A placa não pode ser vazio'})
   plate: string
 
-  @IsInt()
+  @IsInt({message: 'A quilometragem precisa ser um inteiro'})
   @IsNotEmpty({message: 'A quilometragem não pode ser vazio'})
-  @Min(0)
+  @Min(0,{message: 'A quilometragem tem que ser maior ou igual a 0'})
   kilometers: number
 
   @IsInt({message: 'O ano precisa ser um inteiro'})
