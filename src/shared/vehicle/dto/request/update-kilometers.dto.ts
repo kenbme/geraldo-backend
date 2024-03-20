73ca29e7d2158737a571e622502d63b89732277b
ViniciusB-albuquerque <vinicius.albuquerque@ccc.ufcg.edu.br>
2024-03-19 21:45:36 -0300

    updadeKmDto refatorado. Mensagens do decorator e propriedades.

--- src/shared/vehicle/dto/request/update-kilometers.dto.ts
+++ src/shared/vehicle/dto/request/update-kilometers.dto.ts
@@ -1,7 +1,8 @@
-import {IsInt, Min} from 'class-validator'
+import {IsInt, IsNotEmpty, Min} from 'class-validator'
 
 export class UpdateKilometersDto {
-  @IsInt()
-  @Min(0)
+  @IsInt({message: 'A quilometragem precisa ser um inteiro'})
+  @IsNotEmpty({message: 'A quilometragem não pode ser vazio'})
+  @Min(0,{message: 'A quilometragem tem que ser maior ou igual a 0'})
   kilometers: number
 }
