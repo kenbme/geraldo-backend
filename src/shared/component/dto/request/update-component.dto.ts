0f41382e1a1ea6b2cb49c2dd1cf798ef18ce1480
Thalles Vasconcelos <thalles.vasconcelos@ccc.ufcg.edu.br>
2024-03-19 21:45:23 -0300

    fix: adicionado atributo notempty para update component

--- src/shared/component/dto/request/update-component.dto.ts
+++ src/shared/component/dto/request/update-component.dto.ts
@@ -1,16 +1,20 @@
-import {IsInt} from 'class-validator'
+import {IsInt, IsNotEmpty} from 'class-validator'
 import {IsDateValid} from '../../validators/IsDateValid'
 
 export class UpdateComponentDto {
+  @IsNotEmpty()
   @IsDateValid()
   dateLastExchange: string
 
+  @IsNotEmpty()
   @IsInt()
   kilometersLastExchange: number
 
+  @IsNotEmpty()
   @IsInt()
   maintenanceFrequency: number
 
+  @IsNotEmpty()
   @IsInt()
   vehicleId: number
 }
