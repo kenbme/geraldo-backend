3218fb6e8844fae151d95e5271110b22b6fdc7e5
Thalles Vasconcelos <thalles.vasconcelos@ccc.ufcg.edu.br>
2024-03-19 21:35:33 -0300

    "fix: adicionado atributo notEmpty aos request"

--- src/shared/vehicle/dto/request/update-kilometers.dto.ts
+++ src/shared/vehicle/dto/request/update-kilometers.dto.ts
@@ -1,9 +1,11 @@
-import {IsInt, Min} from 'class-validator'
+import {IsInt, IsNotEmpty, Min} from 'class-validator'
 
 export class UpdateKilometersDto {
+  @IsNotEmpty()
   @IsInt()
   vehicleId: number
 
+  @IsNotEmpty()
   @IsInt()
   @Min(0)
   kilometers: number
