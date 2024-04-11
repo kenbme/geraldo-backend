afc961a147f224af775896c1fd12b5c11ce97ede
joaopedrocporto <jpcamposporto@gmail.com>
2024-04-11 20:30:58 -0300

    correcting grade

--- src/shared/avaliation/dto/request/create_avaliation.dto.ts
+++ src/shared/avaliation/dto/request/create_avaliation.dto.ts
@@ -1,10 +1,11 @@
-import { IsNotEmpty, Max, Min } from "class-validator"
+import { IsInt, IsNotEmpty, Max, Min } from "class-validator"
 
 export class CreateAvaliationDto{
     comment: string
     @Min(0,{message: "A nota nao ode ser inferior a 0"}) 
     @Max(5, {message: "A nota nao pode ser superior a 5"})
     @IsNotEmpty({message: "A nota nao pode ser vazia"})
+    @IsInt({message: "A nota deve ser um número inteiro"})
     grade: number
     date: Date
 }
