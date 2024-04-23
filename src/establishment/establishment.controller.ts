0a5138af49fda1844953104175b921245e20e1c9
kenbme <guilherme.barbosa@ccc.ufcg.edu.br>
2024-04-23 00:21:57 -0300

    feat: rf23 e rf16 usando a função de João Pedro(pegar estabelecimentos próximos)

--- src/establishment/establishment.controller.ts
+++ src/establishment/establishment.controller.ts
@@ -1,31 +1,31 @@
 
-import {Controller, Post, Body, Param, Put, Request, UnauthorizedException, BadRequestException, Get} from '@nestjs/common'
+import { Controller, Post, Body, Put, Request, UnauthorizedException, Get, Query } from '@nestjs/common'
 
-import {EstablishmentService} from './establishment.service'
-import {CreateEstablishmentDto} from '../shared/establishment/dto/request/create-establishment.dto'
-import {EstablishmentResponseDTO} from '../shared/establishment/dto/response/establishment.response.dto'
-import {createEstablishmentResponseDTO} from '../util/mapper'
+import { EstablishmentService } from './establishment.service'
+import { CreateEstablishmentDto } from '../shared/establishment/dto/request/create-establishment.dto'
+import { EstablishmentResponseDTO } from '../shared/establishment/dto/response/establishment.response.dto'
+import { createEstablishmentResponseDTO } from '../util/mapper'
 
-import {Public, Roles} from '../config/decorator'
-import { UpdateEstablishmentDto } from 'src/shared/establishment/dto/request/update-establishment.dto'
-import { UserTypeEnum } from 'src/shared/user/enums/user-type.enum'
-import { Establishment } from './entities/establishment.entity'
-import { UserRequest } from 'src/shared/auth/dto/user.request'
-import { FuelTypeEnum } from 'src/shared/fuel/enum/fuel.type.enum'
+import { Public, Roles } from '../config/decorator'
+import { UpdateEstablishmentDto } from '../shared/establishment/dto/request/update-establishment.dto'
+import { UserTypeEnum } from '../shared/user/enums/user-type.enum'
+import { UserRequest } from '../shared/auth/dto/user.request'
+import { GetEstablishmentsQuery } from '../shared/establishment/dto/request/get-establishments-query'
+import { GetEstablishmentsOrderedByPriceQuery } from '../shared/establishment/dto/request/get-establishments-ordered-query'
 
 
 @Controller('')
 export class EstablishmentController {
-  constructor(private readonly establishmentService: EstablishmentService) {}
+  constructor(private readonly establishmentService: EstablishmentService) { }
 
   @Post('/establishment_register')
   @Public()
   async create(
     @Body() createEstablishmentDto: CreateEstablishmentDto
-  ): Promise<{data: EstablishmentResponseDTO; message: string}> {
+  ): Promise<{ data: EstablishmentResponseDTO; message: string }> {
     const establishment = await this.establishmentService.create(createEstablishmentDto)
     const data = createEstablishmentResponseDTO(establishment)
-    return {data, message: 'Estabelecimento cadastrado com sucesso'}
+    return { data, message: 'Estabelecimento cadastrado com sucesso' }
   }
 
   @Roles(UserTypeEnum.ESTABLISHMENT)
@@ -33,43 +33,34 @@ export class EstablishmentController {
   async updateEstablishment(
     @Request() request: UserRequest,
     @Body() updateEstablishmentDto: UpdateEstablishmentDto
-  ): Promise<{data: EstablishmentResponseDTO; message: string}>{
+  ): Promise<{ data: EstablishmentResponseDTO; message: string }> {
     const userId = request.user.id
     if (!userId) {
       throw new UnauthorizedException()
     }
     const establishment = await this.establishmentService.updateEstablishment(userId, updateEstablishmentDto)
     const data = createEstablishmentResponseDTO(establishment)
-    return {data, message: 'Estabelecimento atualizado com sucesso'}
-  }
-  
-  @Roles(UserTypeEnum.DRIVER)
-  @Get("/establishments/:cityId")
-  async getEstablishments(
-    @Param("cityId") cityId?: string
-  ): Promise<{data: EstablishmentResponseDTO[], message: string}> {
-    if (!cityId) {
-      throw new BadRequestException("Cidade não é válida")
-    }
-    const establishments = await this.establishmentService.getEstablishments(parseInt(cityId))
-    const data = establishments.map((it) => createEstablishmentResponseDTO(it))
-    return {data, message: 'Lista de estabelecimentos'}
+    return { data, message: 'Estabelecimento atualizado com sucesso' }
   }
 
   @Roles(UserTypeEnum.DRIVER)
-  @Get("/establishments/:cityId")
-  async getEstablishmentsOrderedByPrice(
-    @Param("cityId") cityId?: string,
-    @Param("fuelType") fuelType?: string
-  ): Promise<{data: EstablishmentResponseDTO[], message: string}> {
-    if (!cityId) {
-      throw new BadRequestException("Cidade não é válida")
-    }
-    if(!fuelType || !(Object.values(FuelTypeEnum).includes(fuelType as FuelTypeEnum))) {
-      throw new BadRequestException("Combustivel não é válido")
-    }
-    const establishments = await this.establishmentService.getEstablishmentsOrderedByPrice(parseInt(cityId), fuelType as FuelTypeEnum)
+  @Get("/establishments")
+  async getEstablishments(
+    @Query() query: GetEstablishmentsQuery
+  ): Promise<{ data: EstablishmentResponseDTO[], message: string }> {
+    const establishments = await this.establishmentService.getEstablishments(query.latitude, query.longitude)
     const data = establishments.map((it) => createEstablishmentResponseDTO(it))
-    return {data, message: 'Lista de estabelecimentos ordenados por preço'}
+    return { data, message: 'Lista de estabelecimentos' }
+  }
+
+  @Roles(UserTypeEnum.DRIVER)
+  @Get("/establishments_by_price")
+  async getEstablishmentsOrderedByPrice(
+    @Query() query: GetEstablishmentsOrderedByPriceQuery
+  ): Promise<{ data: EstablishmentResponseDTO[], message: string }> {
+    const establishments = await this.establishmentService.
+      getEstablishmentsOrderedByPrice(query.latitude, query.longitude, query.fuelType)
+    const data = establishments.map((it) => createEstablishmentResponseDTO(it))
+    return { data, message: 'Lista de estabelecimentos ordenados por preço' }
   }
 }
---
+++ src/shared/establishment/dto/request/get-establishments-ordered-query.ts
@@ -0,0 +1,20 @@
+import {IsNotEmpty, IsNumber, Min, Max, IsEnum} from 'class-validator'
+import { FuelTypeEnum } from 'src/shared/fuel/enum/fuel.type.enum';
+
+export class GetEstablishmentsOrderedByPriceQuery{
+  @IsNotEmpty({message: 'Campo obrigatório'})
+  @IsEnum(FuelTypeEnum)
+  fuelType: FuelTypeEnum
+
+  @IsNotEmpty({message: 'Campo obrigatório'})
+  @IsNumber()
+  @Min(-90)
+  @Max(90)
+  latitude: number;
+
+  @IsNotEmpty({message: 'Campo obrigatório'})
+  @IsNumber()
+  @Min(-180)
+  @Max(180)
+  longitude: number;
+}
